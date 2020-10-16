b886a02e34fef8fd0952689c4014af6a73e0b279
Félix Étienne Labrie <fel.labrie@live.ca>
2020-10-16 13:20:49 -0400

    Add project files.

---
+++ PasswordException/PasswordException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordException
+{
+    class PasswordException : ApplicationException
+    {
+        private string messageDetails = String.Empty;
+
+        public override string Message
+        {
+            get
+            {
+                return "A password error occurred: " + messageDetails;
+            }
+        }
+
+        public string PasswordRuleIssue = String.Empty;
+
+        public int Strength = 0;
+
+        public PasswordException() { }
+
+        public PasswordException(string message, string pwIssue, int strength) 
+        {
+            messageDetails = message;
+            PasswordRuleIssue = pwIssue;
+            Strength = strength;
+        }
+
+
+
+
+
+    }
+}
---
+++ PasswordException/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordException
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your password: ");
+            string pass = Console.ReadLine();
+
+            try
+            {
+
+                validatePass(pass);
+            }
+            catch (PasswordException pwe)
+            {
+                Console.WriteLine("A password error occurred.");
+                Console.WriteLine(pwe.Message);
+                Console.WriteLine(pwe.PasswordRuleIssue);
+                Console.WriteLine("Strength: " + pwe.Strength);
+            }
+            finally
+            {
+                Console.WriteLine("The access code is ABCDE123");
+            }
+        }
+
+        static bool validatePass(string password)
+        {
+            if (password.Length < 8)
+            {
+                var pe = new PasswordException("A password issue occurred", "Length was too short", 3);
+                throw pe;
+            }
+
+            return true;
+        }
+    }
+}
